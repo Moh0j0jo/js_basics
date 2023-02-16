a42c83f04865807035f5ff5c5a02099778f41df1
Moh0j0jo <pinnel.peter@gmail.com>
2023-02-16 15:31:29 +0100

    practice 2

--- script.js
+++ script.js
@@ -10,48 +10,28 @@ let favoriteBooks = [
         author: "Romhányi József",
         year: 2003,
         isNewerThan2000: true
-    },
-
-    {
-      title: "BookNev3",
-      author: "Pelda Iro3",
-      year: 2003,
-      isNewerThan2000: true
-  },
-
-    {
-      title: "BookNev4",
-      author: "Pelda Iro4",
-      year: 2003,
-      isNewerThan2000: true
-  },
-
-  {
-    title: "BookNev5",
-    author: "Pelda Iro5",
-    year: 2003,
-    isNewerThan2000: true
-},
-
-{
-  title: "BookNev6",
-  author: "Pelda Iro6",
-  year: 2003,
-  isNewerThan2000: true
-},
-
-{
-  title: "BookNev7",
-  author: "Pelda Iro7",
-  year: 2003,
-  isNewerThan2000: true
-},
+    }
 ]
 
 for (let index = 0; index < favoriteBooks.length; index++) {
   const author = favoriteBooks[index].author;
-  const title = favoriteBooks[index].title;
-  console.log(`Konyv: ${title} Iroja: ${author}`);
-}
+  let title = favoriteBooks[index].title;
+  const ischeck = favoriteBooks[index].isNewerThan2000;
+  const year = favoriteBooks[index].year;
+  let age = ""
+  
+  // if (ischeck){
+  //   age = "newer"
+  //  } else {
+  //   age = "older"
+  // }
+  // console.log(`Book: ${title} This book is ${age} than 2000`);
 
- 
+  if (year > 2000){
+    age = "newer"
+   } else {
+    age = "older"
+  }
+  console.log(`Book: ${title} This book is ${age} than 2000`);
+};
+  
